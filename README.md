c0dc6505e58ceafaef98377b14660a2a12f4f228
TanelTM <taneltm@users.noreply.github.com>
2023-11-22 01:50:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+GitHub Copilot: Sure, here's a basic structure for a README.md file for your project:
+
+```markdown
+# Passifist
+
+Passifist is a password manager built with Deno. It uses SHA-512 for hashing and provides a terminal interface for user interaction.
+
+## Installation
+
+1. Ensure you have [Deno installed](https://deno.land/#installation) on your machine.
+2. Clone the repository to your local machine.
+3. Navigate to the project directory.
+
+## Usage
+
+1. Run the `main.ts` file with Deno:
+
+```sh
+deno run --allow-run main.ts
+```
+
+This will prompt you to enter your master password, pin, and password index.
+
+## Testing
+
+To run the tests for the project, use the following command:
+
+```sh
+deno test main_test.ts
+```
+
+## Contributing
+
+Contributions are welcome. Please make sure to update tests as appropriate.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+```
+
+Please replace the placeholders with the actual information about your project.
---
+++ main.ts
@@ -0,0 +1,75 @@
+// Draw a terminal menu using deno
+import { Select, Secret } from "https://deno.land/x/cliffy/prompt/mod.ts";
+
+const PASS_LENGTH = 12
+
+const masterPass = await Secret.prompt("Enter your password");
+const pin = Number(await Secret.prompt("Enter your pin"));
+const passIndex = clamp(Number(await Secret.prompt("Enter the password index (0-9)")), 0, 9);
+
+function clamp(num: number, min: number, max: number) {
+  return Math.min(Math.max(num, min), max);
+}
+
+
+async function sha512(message: string) {
+  // encode as UTF-8
+  const msgBuffer = new TextEncoder().encode(message);                    
+
+  // hash the message
+  const hashBuffer = await crypto.subtle.digest('SHA-512', msgBuffer);
+
+  // convert ArrayBuffer to Array
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+  // convert bytes to hex string                  
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+  return hashHex;
+}
+
+const a = await sha512(masterPass)
+
+const b = (() => {
+  if (!pin) return a
+
+  const shiftBy = pin % 26
+
+  return a.replace(/[a-z]/g, (char) => {
+    const charCode = char.charCodeAt(0)
+      
+    return String.fromCharCode(
+      ((charCode + shiftBy) <= 122)
+        ? charCode + shiftBy
+        : (charCode + shiftBy) % 122 + 96
+    )
+  })
+})()
+
+const c = (
+  [...b]
+  .map((char, i) => (
+    (i % 2)
+      ? char.toUpperCase()
+      : char
+  ))
+  .join('')
+ )
+
+const d = c.match(new RegExp('.{1,' + PASS_LENGTH + '}', 'g'))!
+
+async function copyToClipboard(text: string) {
+  const p = Deno.run({
+    cmd: ['clip'],
+    //cmd: ['powershell', '-noprofile', '-command', '$input|Set-Clipboard'],
+    stdin: "piped",
+  });
+
+  await p.stdin.write(new TextEncoder().encode(text));
+  p.stdin.close();
+  await p.status();
+  p.close();
+}
+
+await copyToClipboard(d[passIndex])
+await new Promise(resolve => setTimeout(resolve, 5000))
+await copyToClipboard('')
